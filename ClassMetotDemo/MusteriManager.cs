a2006965b33629dc90629d2d33f35d441c34e178
keremlas <legolaskerem579@hotmail.com>
2021-01-18 14:10:00 +0300

    v0.1

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public static void Add(List<Musteri> musteriler, bool adding)
+        {
+            while (adding)
+            {
+                var yeniMusteri = new Musteri();
+                Console.WriteLine("Lütfen müşterinin ID'sini giriniz.");
+                yeniMusteri.Id = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Lütfen müşterinin adını ve soyadını giriniz.");
+                yeniMusteri.adSoyad = Console.ReadLine();
+                musteriler.Add(yeniMusteri);
+                Console.WriteLine("\nMüşteri ID: {0}, Müşteri ad ve soyadı: {1}\n", yeniMusteri.Id, yeniMusteri.adSoyad);
+                Console.WriteLine("Yeni müşteri eklemek istiyor musunuz? e/h");
+                var addingAnswer = Console.ReadLine();
+                if (addingAnswer == "h")
+                {
+                    adding = false;
+                }
+            }
+
+
+        }
+        public static void Delete(List<Musteri> musteriler, bool deleting)
+        {
+            while (deleting)
+            {
+                Console.WriteLine("Kaçıncı müşteriyi silmek istiyorsunuz?");
+                int countAnswer = int.Parse(Console.ReadLine()) - 1;
+                musteriler.Remove(musteriler[countAnswer]);
+                Console.WriteLine("Başka müşteri silmek ister misiniz? e/h");
+                var deletingAnswer = Console.ReadLine();
+
+                if (deletingAnswer != "e")
+                {
+                    deleting = false;
+                }
+            }
+
+        }
+        public static void List(List<Musteri> musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("------------------------------------------------------------------------");
+                Console.WriteLine("Müşteri ID: {0}, Müşteri ad ve soyadı: {1}", musteri.Id, musteri.adSoyad);
+                Console.WriteLine("------------------------------------------------------------------------");
+            }
+        }
+
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -15,7 +15,6 @@ namespace ClassMetotDemo
                 Console.WriteLine("Hangi işlemi yapmak istiyorsunuz? müşteri ekle: 1/müşteri sil: 2/müşterileri listele: 3/çık: 4");
                 var answer = Console.ReadLine();
                 
-                
                 if (answer=="1")
                 {
                     MusteriManager.Add(musteriler, adding);
@@ -44,63 +43,7 @@ namespace ClassMetotDemo
         }
     }
 
-    class Musteri
-    {
-        public int Id;
-        public string adSoyad; 
-    }
+    
 
-    class MusteriManager
-    {
-        
-        public static void Add(List<Musteri> musteriler, bool adding)
-        {
-            while(adding)
-            {
-                var yeniMusteri = new Musteri();
-                Console.WriteLine("Lütfen müşterinin ID'sini giriniz.");
-                yeniMusteri.Id = int.Parse(Console.ReadLine());        
-                
-                Console.WriteLine("Lütfen müşterinin adını ve soyadını giriniz.");
-                yeniMusteri.adSoyad = Console.ReadLine();
-                musteriler.Add(yeniMusteri);
-                Console.WriteLine("\nMüşteri ID: {0}, Müşteri ad ve soyadı: {1}\n", yeniMusteri.Id, yeniMusteri.adSoyad);
-                Console.WriteLine("Yeni müşteri eklemek istiyor musunuz? e/h");
-                var addingAnswer = Console.ReadLine();
-                if (addingAnswer == "h")
-                {
-                    adding = false;
-                }
-            }
-            
-            
-        }
-        public static void Delete(List<Musteri> musteriler, bool deleting)
-        {
-            while(deleting)
-            {
-                Console.WriteLine("Kaçıncı müşteriyi silmek istiyorsunuz?");
-                int countAnswer = int.Parse(Console.ReadLine()) - 1;
-                musteriler.Remove(musteriler[countAnswer]);
-                Console.WriteLine("Başka müşteri silmek ister misiniz? e/h");
-                var deletingAnswer = Console.ReadLine();
-                
-                if (deletingAnswer != "e")
-                {
-                    deleting = false;
-                }
-            }
-            
-        }
-        public static void List(List<Musteri> musteriler)
-        {
-            foreach (var musteri in musteriler)
-            {
-                Console.WriteLine("------------------------------------------------------------------------");
-                Console.WriteLine("Müşteri ID: {0}, Müşteri ad ve soyadı: {1}", musteri.Id, musteri.adSoyad);
-                Console.WriteLine("------------------------------------------------------------------------");
-            }
-        }
-        
-    }
+    
 }
