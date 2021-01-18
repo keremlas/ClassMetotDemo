ba9a4d87b16f037576222647e1a0ae6a5839f17f
keremlas <legolaskerem579@hotmail.com>
2021-01-18 14:10:04 +0300

    Create Musteri.cs

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+        public int Id;
+        public string adSoyad;
+    }
+}
